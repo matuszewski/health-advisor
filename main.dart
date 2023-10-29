2c0ae1f40856294d04c920005ca63546c5231c0b
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2023-10-29 08:58:43 +0100

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BMICalculatorApp());
+
+class BMICalculatorApp extends StatelessWidget {
+  const BMICalculatorApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const BMICalculator(),
+        '/bmr': (context) => const BMRCalculator(),
+        '/bmi': (context) => const BMICalculator(), // Add this route
+      },
+    );
+  }
+}
+
+class BMICalculator extends StatefulWidget {
+  const BMICalculator({Key? key});
+
+  @override
+  _BMICalculatorState createState() => _BMICalculatorState();
+}
+
+class _BMICalculatorState extends State<BMICalculator> {
+  double weight = 70.0; // Initial weight in kilograms
+  double height = 170.0; // Initial height in centimeters
+  double bmi = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Kalkualtor BMI'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Wpisz swoje dane:',
+              style: TextStyle(fontSize: 18),
+            ),
+            // Weight input
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Slider(
+                  value: weight,
+                  min: 25.0,
+                  max: 200.0,
+                  onChanged: (newValue) {
+                    setState(() {
+                      weight = newValue;
+                      calculateBMI();
+                    });
+                  },
+                ),
+                const Text('waga: '),
+                Text(weight.toStringAsFixed(1)),
+                const Text(' kg'),
+              ],
+            ),
+            // Height input
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Slider(
+                  value: height,
+                  min: 50.0,
+                  max: 230.0,
+                  onChanged: (newValue) {
+                    setState(() {
+                      height = newValue;
+                      calculateBMI();
+                    });
+                  },
+                ),
+                const Text('wysokość:'),
+                Text(height.toStringAsFixed(1)),
+              ],
+            ),
+            Text('Twoje BMI: ${bmi.toStringAsFixed(1)}'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/bmr');
+        },
+        child: const Icon(Icons.accessibility_new),
+      ),
+    );
+  }
+
+  void calculateBMI() {
+    final heightInMeters = height / 100;
+    bmi = weight / (heightInMeters * heightInMeters);
+  }
+}
+
+class BMRCalculator extends StatefulWidget {
+  const BMRCalculator({Key? key});
+
+  @override
+  _BMRCalculatorState createState() => _BMRCalculatorState();
+}
+
+class _BMRCalculatorState extends State<BMRCalculator> {
+  int age = 25;
+  double weight = 70.0; // Initial weight in kilograms
+  double height = 170.0; // Initial height in centimeters
+  String gender = 'Male';
+  double bmr = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Kalkualtor BMR'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Wpisz swoje dane:',
+              style: TextStyle(fontSize: 18),
+            ),
+            // Age input
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Text('Wiek:'),
+                Slider(
+                  value: age.toDouble(),
+                  min: 0,
+                  max: 100,
+                  onChanged: (newValue) {
+                    setState(() {
+                      age = newValue.toInt();
+                      calculateBMR();
+                    });
+                  },
+                ),
+                Text(age.toString()),
+              ],
+            ),
+            // Gender selection
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Text('Płeć:'),
+                Radio<String>(
+                  value: 'Mężczyzna',
+                  groupValue: gender,
+                  onChanged: (value) {
+                    setState(() {
+                      gender = value!;
+                      calculateBMR();
+                    });
+                  },
+                ),
+                const Text('Mężczyzna'),
+                Radio<String>(
+                  value: 'Kobieta',
+                  groupValue: gender,
+                  onChanged: (value) {
+                    setState(() {
+                      gender = value!;
+                      calculateBMR();
+                    });
+                  },
+                ),
+                const Text('Kobieta'),
+              ],
+            ),
+            // Weight input
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Text('Waga:'),
+                Slider(
+                  value: weight,
+                  min: 0.0,
+                  max: 150.0,
+                  onChanged: (newValue) {
+                    setState(() {
+                      weight = newValue;
+                      calculateBMR();
+                    });
+                  },
+                ),
+                Text(weight.toStringAsFixed(1)),
+              ],
+            ),
+            // Height input
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Text('Wysokość:'),
+                Slider(
+                  value: height,
+                  min: 0.0,
+                  max: 220.0,
+                  onChanged: (newValue) {
+                    setState(() {
+                      height = newValue;
+                      calculateBMR();
+                    });
+                  },
+                ),
+                Text(height.toStringAsFixed(1)),
+              ],
+            ),
+            Text('Twoje BMR: ${bmr.toStringAsFixed(1)} kcal/dziennie'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void calculateBMR() {
+    if (gender == 'Mężczyzna') {
+      bmr = 66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age);
+    } else {
+      bmr = 655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age);
+    }
+  }
+}
